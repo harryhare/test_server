9a0984e6343a48c18b919f57401867612769ee22
harryhare <harryhare@163.com>
2020-02-18 21:52:10 +0800

    fix headers

--- server.py
+++ server.py
@@ -3,11 +3,16 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 
 class handler1(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path != "/favicon.ico":
+        if self.path == "/favicon.ico":
+            self.send_response(404)
+            self.end_headers()
+        else:
             print("path:", self.path)
             print(self.request)
             print(self.headers)
             self.send_response(200)
+            #self.send_header("Content-type", "application/json")
+            self.end_headers()
             self.wfile.write(b"hello1")
 
 
